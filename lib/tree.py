9e608d3285f2b7ccadb8c66d7c52c1f1bc4285eb
Annania Plossl <nia.plossl@gmail.com>
2024-04-29 12:56:29 -0400

    completed the lab

--- lib/tree.py
+++ lib/tree.py
@@ -3,4 +3,9 @@ class Tree:
     self.root = root
 
   def get_element_by_id(self, id):
-    pass
+    elements_to_check = [self.root]
+    while len(elements_to_check):
+      element = elements_to_check.pop()
+      if element["id"] == id:
+        return element
+      elements_to_check += element["children"]
